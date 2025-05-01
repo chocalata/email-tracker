d3ec427aa02bc075a51804ced7a64af97077e446
chocalata <cata627@gmail.com>
2025-05-01 13:03:58 +0200

    Update build and Docker configurations: add node-canvas dependencies, enhance tracking middleware with color handling, and update tracker name placeholder in the UI.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
-docker build ./email-tracker -t chocalata/email-tracker.catalin-trandafir.com:app-2025-04-27-v2
+docker build ./email-tracker -t chocalata/email-tracker.catalin-trandafir.com:app-2025-05-01-v1
 
-docker push chocalata/email-tracker.catalin-trandafir.com:app-2025-04-27-v2
+docker push chocalata/email-tracker.catalin-trandafir.com:app-2025-05-01-v1
--- email-tracker/src/middlewares/tracking.js
+++ email-tracker/src/middlewares/tracking.js
@@ -1,25 +1,38 @@
-const path = require('path')
+const { createCanvas } = require('canvas') // npm install canvas
 
 const trackingService = require('../service/tracking-service')
 const { getErrorMessage } = require('../utils/json-responses')
 
-const IMAGE_PATH = path.join(__dirname, '..', 'tracking-image.png')
-
 const checkTracking = async (req, res, next) => {
   const tracking = req.query.tracking
+  const color = `#${req.query.color || 'fff'}`
   const trackingId = req.params.trackingId
 
   if (!trackingId) {
     return res
       .status(400)
-      .send(
+      .json(
         getErrorMessage('Tracking ID is required (/tracking/image/:trackingId)')
       )
   }
 
-  if (tracking === 'off') return res.sendFile(IMAGE_PATH)
+  if (!colorFormat(color)) {
+    return res
+      .status(400)
+      .json(
+        getErrorMessage(
+          'Color format is invalid. Has to be hex and 3 or 6 characters.'
+        )
+      )
+  }
 
-  res.sendFile(IMAGE_PATH)
+  const pixelBuffer = getPixelColor(color)
+
+  res.setHeader('Content-Type', 'image/png')
+
+  if (tracking === 'off') return res.send(pixelBuffer)
+
+  res.send(pixelBuffer)
 
   const trackingData = await trackingService.getTrackingData(trackingId)
 
@@ -30,6 +43,21 @@ const checkTracking = async (req, res, next) => {
     req.trackingData = trackingData
     next()
   }
+}
+
+const colorFormat = (color) => {
+  const hexColorRegex = /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/
+  return hexColorRegex.test(color)
+}
+
+const getPixelColor = (color) => {
+  const canvas = createCanvas(1, 1)
+  const ctx = canvas.getContext('2d')
+
+  ctx.fillStyle = color
+  ctx.fillRect(0, 0, 1, 1)
+
+  return canvas.toBuffer('image/png')
 }
 
 module.exports = { checkTracking }
