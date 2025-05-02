015d7df395e11bf61580c65cce59e80debf0299c
chocalata <cata627@gmail.com>
2025-05-02 17:15:04 +0200

    Added meta tags and favicon

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
-docker build ./email-tracker -t chocalata/email-tracker.catalin-trandafir.com:app-2025-05-01-v1
+docker build ./email-tracker -t chocalata/email-tracker.catalin-trandafir.com:app-2025-05-02-v3
 
-docker push chocalata/email-tracker.catalin-trandafir.com:app-2025-05-01-v1
+docker push chocalata/email-tracker.catalin-trandafir.com:app-2025-05-02-v3
